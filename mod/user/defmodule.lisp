bc85fa8d875b3b3684759ec90c0af50ffd496943
Rigidus <avenger-f@yandex.ru>
2013-03-13 07:12:00 +0400

    add tests and macro changes

--- mod/user/defmodule.lisp
+++ mod/user/defmodule.lisp
@@ -1,13 +1,17 @@
 
-(define-automat user "Автомат пользователя"
-  ((email        :email)
-   (password     :password)
-   (new-password :password))
-  (:logged :unlogged :link-sended)
-  ((:logged       :unlogged     :leave       "Обнулить сессию")
-   (:unlogged     :logged       :enter       "Залогиниться")
-   (:unlogged     :link-sended  :send-login  "Забыл пароль - пошлем линк")
-   (:link-sended  :logged       :enter       "Залогиниться")))
+;; (print
+;;  (macroexpand-1
+;;   '
+  (define-automat user "Автомат пользователя"
+    ((email        :email)
+     (password     :password)
+     (new-password :password))
+    (:logged :unlogged :link-sended)
+    ((:logged       :unlogged     :leave)       ;; Обнулить сессию
+     (:unlogged     :logged       :enter)       ;; Залогиниться
+     (:unlogged     :link-sended  :send-login)  ;; Забыл пароль - пошлем линк
+     (:link-sended  :logged       :enter)))     ;; Залогиниться
+;; ))
 
 
 (defun generate-password ()
@@ -15,13 +19,14 @@
 
 (defun registration (email)
   ;; TODO: Проверяеть email на валидность, если не валиден - сигнализировать err-param
-  ;; TODO: Проверять, есть ли уже такой email, если есть - возвращать nil
+  ;; Если есть уже такой email - возвращать nil
+  (when (get-account email)
+    (return-from registration nil))
   (make-user :email email :password (generate-password) :state :logged))
 
 (defun delete-account (email)
   ;; TODO: Проверять права, если проверка не прошла - сигнализировать err-permission
-  (let ((match (find-user #'(lambda (x)
-                              (equal (email (car x)) email)))))
+  (let ((match (find-user #'(lambda (x) (equal (email (car x)) email)))))
     (if (null match)
         nil
         (prog1 t
@@ -30,9 +35,25 @@
                   match)))))
 
 (defun get-account (email)
-  (caar
-   (find-user #'(lambda (x)
-                  (equal (email (car x)) email)))))
+  (caar (find-user #'(lambda (x)
+                       (equal (email (car x)) email)))))
+
+(defun all-accounts ()
+  (all-user))
+
+(define-action enter (email password)
+  (let ((data (get-account email)))
+    (when (null data)
+      (return-from enter nil))
+    (when (or (string= password (password data))
+              (string= password (new-password data)))
+      (setf (password data) password)
+      (setf (new-password data) "")
+      (setf *current-user* data)
+      t)))
+
+(define-action leave ()
+  t)
 
 
 ;; Tests
@@ -45,6 +66,9 @@
 
 ;; Регистрация пользователя_3 — успешно
 (assert (equal 'user (type-of (registration "user_3@example.tld"))))
+
+;; Регистрация пользователя_4 — успешно
+(assert (equal 'user (type-of (registration "user_4@example.tld"))))
 
 ;; Попытка регистрации пользователя_2 (повторная) — неуспешно
 (assert (equal nil (registration "user_2@example.tld")))
@@ -61,34 +85,29 @@
 ;; Получение аккаунта пользователя_3 — успешно
 (assert (equal 'user (type-of (get-account "user_3@example.tld"))))
 
-;; Получение всех аккаунтов — существуют аккаунты пользователя_2 и пользователя_3, аккаунт пользователя_1 — не существует
-;; Вход в систему пользователя_2 — успешно
-;; Вход в систему пользователя_3 — успешно
-;; Попытка входа в систему пользователя_1 — неуспешно
-;; Проверка состояния аккаунта пользователя_2 - залогинен
-;; Выход из системы пользователя_2 — успешно
-;; Отсылка логина пользователю_3 — успешно
-;; Получение всех пользователей и анализ из состояния — пользователь_2 незалогинен, пользователь_3 - послан пароль
+;; Получение всех аккаунтов — существуют аккаунты пользователя_2, пользователя_3, пользователя_4, а аккаунт пользователя_1 — не существует
+(assert (equal '("user_2@example.tld" "user_3@example.tld" "user_4@example.tld")
+               (mapcar #'(lambda (x)
+                           (email (car x)))
+                       (all-accounts))))
 
+;; Выход из системы пользователя_3 — успешно
+(assert (equal t (takt (get-account "user_3@example.tld") :unlogged :leave)))
 
-(define-automat user-auth "Автомат авторизации пользователя"
-  (:logged :unlogged :link-sended :logged-for-change-password)
-  ((:logged       :unlogged     :leave       "Обнулить сессию")
-   (:unlogged     :logged       :enter       "Залогиниться")
-   (:unlogged     :link-sended  :send-login  "Забыл пароль - пошлем линк")
-   (:link-sended  :logged       :enter       "Залогиниться")))
+;; Выход из системы пользователя_4 — успешно
+(assert (equal t (takt (get-account "user_4@example.tld") :unlogged :leave)))
 
-(define-action enter (GET token)
-  (request-protect
-   (let ((data (get-user token)))
-     (setf (getf data :state) :logged-out)
-     (destroy-session))))
+;; TODO:
+;; Отсылка пароля пользователю_4 — успешно
+;; Проверка состояния аккаунтов:
+;;     Пользователь_2 - залогинен
+;;     Пользователь_3 - незалогинен
+;;     Пользователь_4 - послан пароль
+;; Отсылка пароля пользователю_3 — успешно
+;; Попытка логина пользователя_3 с неправильным паролем — неуспешно
+;; Попытка логина пользователя_3 с новым паролем — успешно
+;; Попытка логина пользователя_2 со старым паролем — успешно
 
-(define-action leave (POST login password)
-  (request-protect
-   (let ((data (get-user login password)))
-     (setf (getf data :state) :logged-in)
-     (setf (getf session :token) (token data)))))
 
 (define-action send-login (GET email)
   (request-protect
--- mod/user/entity.lisp
+++ mod/user/entity.lisp
@@ -110,232 +110,28 @@
            rs))
        )))
 
+
+(defmacro define-action (name params &body body)
+  `(defun ,name ()
+     (let ,(loop :for param :in params :collect
+              `(,param (getform ,(intern (symbol-name param) :keyword))))
+       ,@body)))
+
+
 (defmacro define-automat (name desc &rest tail)
   `(progn
      (define-entity ,name ,desc
        ,(list* '(state :state) (car tail)))
      ,(let ((all-states (cadr tail)))
            `(progn
-              ,@(loop :for (from-state to-state event body) :in (caddr tail) :collect
+              ,@(loop :for (from-state to-state event) :in (caddr tail) :collect
                    (if (or (null (find from-state all-states))
                            (null (find to-state all-states)))
                        (err (format nil "unknown state: ~A -> ~A" from-state to-state))
                        `(defmethod trans ((obj ,name)
                                           (from-state (eql ,from-state)) (to-state (eql ,to-state))
                                           (event (eql ,event)))
-                          (prog1 ,body
+                          (prog1 (,(intern (symbol-name event)))
                             (setf (state obj) ,to-state)))))
               (defmethod takt ((obj ,name) new-state event)
                 (trans obj (state obj) new-state event))))))
-
-
-
-
-(define-automat widget "Автомат виджета"
-  ((name :str)
-   (descr :str)
-   (good :id)
-   (blogger :id)
-   (date :time)
-   (amount :money)
-   (sale-count :num)
-   (price :money)
-   (blogger-earning :money)
-   (money-earned :money)
-   (domain :str)
-   (layout :str)
-   (color :str)
-   (sale-until :time))
-  (:active :inactive :deleted)
-  ((:active    :inactive  :blogger^widget-switch-off "блоггер: выключить виджет")
-   (:inactive  :active    :blogger^widget-switch-on  "блоггер: включить виджет")
-   (:active    :inactive  :cron^widget-switch-off    "крон: выключить виджет")
-   (:active    :deleted   :blogger^widget-delete     "блоггер: удалить неактивный виджет")
-   (:inactive  :deleted   :blogger^widget-delete     (print "блоггер: удалить неактивный виджет"))))
-
-
-(make-widget
- :name    "Cool Jacket"
- :descr   "Jacket`s widget"
- :date    *cur-date*
- :good    (find-good (caar (find-good #'(lambda (x) (string= "Jacket" (name (car x)))))))
- :blogger 1
- :amount 100
- :sale-count 1
- :state :active
- :price 90
- :blogger-earning 5
- :domain :str)
-
-(make-widget
- :name    "Nice Shirt"
- :descr   "Shirt`s widget"
- :date    *cur-date*
- :good    (find-good (caar (find-good #'(lambda (x) (string= "Shirt" (name (car x)))))))
- :blogger 1
- :amount 100
- :sale-count 1
- :state :inactive
- :price 90
- :blogger-earning 5
- :domain :str)
-
-(make-widget
- :name    "Super Blazer"
- :descr   "Blazer`s widget"
- :date    *cur-date*
- :good    (find-good (caar (find-good #'(lambda (x) (string= "Blazer" (name (car x)))))))
- :blogger 2
- :amount 100
- :sale-count 1
- :state :deleted
- :price 90
- :blogger-earning 5
- :domain :str)
-
-
-
-
-
-
-
-(define-entity buyer "Покупатель"
-  ((name :str)
-   (email :email)
-   (zip :zip)
-   (delivery-addr :str)
-   (reg-date :time)
-   (lastvisit :time)))
-
-(define-entity blogger "Блоггер"
-  ((name :str)
-   (password :password)
-   (email :email)
-   (date :time)
-   (lastvisit :time)
-   (balance :money)
-   (pending :money)
-   (domain :domain)
-   (activkey :activekey)))
-
-(make-blogger
- :name      "Тестовый блоггер №1"
- :password  "blogger1"
- :email     "blogger1"
- :balance   1000
- :pending   2000
- :domain    "http://example2.com")
-
-(make-blogger
- :name      "Тестовый блоггер №2"
- :password  "blogger2"
- :email     "blogger2"
- :balance   1000
- :pending   2000
- :domain    "http://example2.com")
-
-(define-entity op "Оператор заказов"
-  ((name :str)
-   (password :password)))
-
-(make-op
- :name      "Тестовый оператор №1"
- :password  "op1")
-
-(define-entity gm "Модератор товаров"
-  ((name :str)
-   (password :password)))
-
-(make-gm
- :name      "Тестовый модератор товаров №1"
- :password  "op1")
-
-(define-entity adm "Админ"
-  ((name :str)
-   (password :password)))
-
-(make-adm
- :name      "Тестовый админ №1"
- :password  "op1")
-
-
-
-(define-automat order "Заказ"
-  ((date :time)
-   (cancel-flag :boolean)
-   (cancel-previous-state :cancel-previous-state)
-   (cancel-reason :str)
-   (comission-charged :money)
-   (comission-pending :money)
-   (amount :money)
-   (items-count :num)
-   (widget :id)
-   (buyer :id)
-   (key :str))
-  (:created :abandoned :paid :handed-store :handed-post :delivered :unprotested :protested :cancel :canceled)
-  ((:created      :abandoned    :cron^payment-expired   "крон: истек строк оплаты")
-   (:created      :paid         :blogger^paid           "блоггер: оплатить")
-   ;; ----- paid
-   (:paid         :cancel       :blogger^cancel         (prog1 "блоггер: отменить заказ"
-                                                          (setf (cancel-flag obj) t)
-                                                          (setf (cancel-previous-state) :paid)))
-   (:cancel       :paid         :op^cancel-reject       "оператор: отказать в отмене заказа")
-
-   (:paid         :handed-store :op^handed-store        "оператор: купить в магазине")
-   ;; ------ handed-store
-   (:handed-store :cancel       :blogger^cancel         (prog1 "блоггер: отменить заказ"
-                                                          (setf (cancel-flag obj) t)
-                                                          (setf (cancel-previous-state) :handed-store)))
-   (:cancel       :handed-store :op^cancel-reject       "оператор: отказать в отмене заказа")
-   (:handed-store :protested    :someone^protest        "кто-то: опротестовать")
-
-   (:handed-store :handed-post  :op^handed-post         "оператор: присвоить почтовый идентификатор")
-   ;; ----- handed-post
-   (:handed-post  :cancel       :blogger^cancel         (prog1 "блоггер: отменить заказ"
-                                                          (setf (cancel-flag obj) t)
-                                                          (setf (cancel-previous-state) :handed-post)))
-   (:cancel       :handed-post  :op^cancel-reject       "оператор: отказать в отмене заказа")
-   (:handed-post  :protested    :someone^protest        "кто-то: опротестовать")
-
-   (:handed-post  :delivered    :op^delivered           "оператор: доставлен")
-   ;; ------ delivered
-   (:delivered    :cancel       :blogger^cancel         (prog1 "блоггер: отменить заказ"
-                                                          (setf (cancel-flag obj) t)
-                                                          (setf (cancel-previous-state) :delivered)))
-   (:cancel       :delivered    :op^cancel-reject       "оператор: отказать в отмене заказа")
-   (:delivered    :protested    :someone^protest        "кто-то: опротестовать")
-   ;; ----- unprotested & canceled
-   (:delivered    :unprotested  :cron^unprotested       "крон: неопротестован")
-   (:cancel       :canceled     :op^cancel-accept  "оператор: подтвердить отмену")))
-
-
-(loop
-   :for state
-   :in '(:created :abandoned :paid :handed-store :handed-post :delivered :unprotested :protested :cancel :canceled)
-   :do (make-order
-        :state  state
-        :cancel-previous-state :paid
-        :amount 100
-        :buyer  "some_buyer@yahoo.com"
-        :widget 1))
-
-(define-automat cashout "Заявка на вывод средств"
-  ((date :time)
-   (amount :money)
-   (blogger :id))
-  (:created :accepted :rejected)
-  ((:created :rejected :op^cashout-reject "оператор: отказать в выводе")
-   (:created :accepted :op^cashout-accept "оператор: подтвердить вывод")))
-
-
-(loop
-   :for state
-   :in '(:created :accepted :rejected)
-   :do  (make-cashout
-         :state state
-         :amount 100
-         :blogger 1
-         :state :created))
-
-
-(defparameter *actor* (get-blogger 1))
