7dad69c6c70b4fcaf30baca87dc18b46941fc314
Rigidus <avenger-f@yandex.ru>
2013-04-23 15:27:21 +0400

    defune look automat and export functions stub

--- mod/ily/ily.lisp
+++ mod/ily/ily.lisp
@@ -12,12 +12,65 @@
 (in-package #:ily)
 
 (define-automat look "Автомат look-а"
-  ((...fields...))
-  (...states...)
-  (...transitions))
+  ((timestamp :timestamp)
+   (target    :target)
+   (goods     :goods)
+   (votes     :votes)
+   (comments  :comments))
+  (:draft :public :archived)
+  ((:draft   :public    :publish-look)
+   (:public  :archived  :archive-look)))
 
 
-(define-action ....)
+;; Model
+
+(defun make-look ()
+  "cоздание look-а owner-ом")
+
+(defun edit-look ()
+  "редактирование look-а owner-ом")
+
+(defun delete-look ()
+  "удаление look-а owner-ом")
+
+(defun publish-look ()
+  "публикация look-a owner-ом")
+
+(defun archive-look ()
+  "перевод look-а в состояние archive owner-om")
+
+(defun get-look ()
+  "получение look-а (по id)")
+
+(defun all-looks ()
+  "получение всех look-ов (можно выбрать look-и по критериям, например только опубликованные)")
+
+(defun vote-look ()
+  "голосование за look")
+
+;; View/Controller
+
+
+(defun show-create ()
+  "ook")
+
+(defun show-edit ()
+  "ook")
+
+(defun show-look ()
+  "просмотр look-а")
+
+(defun show-look ()
+  "review — просмотр миниатюры look-а")
+
+(defun action-publish ()
+  "ook")
+
+(defun action-delete ()
+  "ook")
+
+(defun action-vote ()
+  "ook")
 
 
 ;; Tests
